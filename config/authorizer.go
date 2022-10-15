b7871c9d06d75eaf21d7764f5551b000355e2faa
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-10-15 18:07:58 +0200

    feat: add config registration to other configurations
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- config/authorizer.go
+++ config/authorizer.go
@@ -2,12 +2,41 @@ package config
 
 import (
 	"fmt"
+	"sync"
 
 	"gopkg.in/yaml.v3"
 
 	"github.com/distribution-auth/auth/auth"
 	"github.com/distribution-auth/auth/auth/authz"
 )
+
+var (
+	authorizerFactoriesMu sync.RWMutex
+	authorizerFactories   = make(map[string]AuthorizerFactory)
+)
+
+// RegisterAuthorizerFactory makes an AuthenticatorFactory available by the provided name in configuration.
+//
+// If RegisterAuthorizerFactory is called twice with the same name or if factory is nil,
+// it panics.
+func RegisterAuthorizerFactory(name string, factory AuthorizerFactory) {
+	authorizerFactoriesMu.Lock()
+	defer authorizerFactoriesMu.Unlock()
+
+	if factory == nil {
+		panic("registering authorizer factory: factory is nil")
+	}
+
+	if _, dup := authorizerFactories[name]; dup {
+		panic("registering authorizer factory: registration called twice for factory " + name)
+	}
+
+	authorizerFactories[name] = factory
+}
+
+func init() {
+	RegisterAuthorizerFactory("default", defaultAuthorizer{})
+}
 
 // Authorizer is the configuration for an auth.Authorizer.
 type Authorizer struct {
@@ -22,36 +51,37 @@ func (c *Authorizer) UnmarshalYAML(value *yaml.Node) error {
 		return err
 	}
 
-	var config AuthorizerFactory
+	authorizerFactoriesMu.RLock()
+	factory, ok := authorizerFactories[rawConfig.Type]
+	authorizerFactoriesMu.RUnlock()
 
-	switch rawConfig.Type {
-	case "default":
-		var factory defaultAuthorizer
-
-		err := decode(rawConfig.Config, &factory)
-		if err != nil {
-			return err
-		}
-
-		config = factory
-
-	default:
-		return fmt.Errorf("unknown authenticator type: %s", rawConfig.Type)
+	if !ok {
+		return fmt.Errorf("unknown authorizer type: %s", rawConfig.Type)
 	}
 
-	c.Config = config
+	err = decode(rawConfig.Config, &factory)
+	if err != nil {
+		return err
+	}
+
+	c.Config = factory
 
 	return nil
 }
 
 // AuthorizerFactory creates a new auth.Authorizer.
 type AuthorizerFactory interface {
+	New() AuthorizerFactory
 	CreateAuthorizer() (auth.Authorizer, error)
 	Validate() error
 }
 
 type defaultAuthorizer struct {
 	AllowAnonymous bool `mapstructure:"allowAnonymous"`
+}
+
+func (c defaultAuthorizer) New() AuthorizerFactory {
+	return defaultAuthorizer{}
 }
 
 func (c defaultAuthorizer) CreateAuthorizer() (auth.Authorizer, error) {
--- config/token_access.go
+++ config/token_access.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"gopkg.in/yaml.v3"
@@ -10,6 +11,34 @@ import (
 	"github.com/distribution-auth/auth/auth/token/jwt"
 	"github.com/docker/libtrust"
 )
+
+var (
+	accessTokenIssuerFactoriesMu sync.RWMutex
+	accessTokenIssuerFactories   = make(map[string]AccessTokenIssuerFactory)
+)
+
+// RegisterAccessTokenIssuerFactory makes an AuthenticatorFactory available by the provided name in configuration.
+//
+// If RegisterAccessTokenIssuerFactory is called twice with the same name or if factory is nil,
+// it panics.
+func RegisterAccessTokenIssuerFactory(name string, factory AccessTokenIssuerFactory) {
+	accessTokenIssuerFactoriesMu.Lock()
+	defer accessTokenIssuerFactoriesMu.Unlock()
+
+	if factory == nil {
+		panic("registering access token issuer factory: factory is nil")
+	}
+
+	if _, dup := accessTokenIssuerFactories[name]; dup {
+		panic("registering access token issuer factory: registration called twice for factory " + name)
+	}
+
+	accessTokenIssuerFactories[name] = factory
+}
+
+func init() {
+	RegisterAccessTokenIssuerFactory("jwt", jwtAccessTokenIssuer{})
+}
 
 // AccessTokenIssuer is the configuration for an auth.AccessTokenIssuer.
 type AccessTokenIssuer struct {
@@ -24,30 +53,27 @@ func (c *AccessTokenIssuer) UnmarshalYAML(value *yaml.Node) error {
 		return err
 	}
 
-	var config AccessTokenIssuerFactory
+	accessTokenIssuerFactoriesMu.RLock()
+	factory, ok := accessTokenIssuerFactories[rawConfig.Type]
+	accessTokenIssuerFactoriesMu.RUnlock()
 
-	switch rawConfig.Type {
-	case "jwt":
-		var factory jwtAccessTokenIssuer
-
-		err := decode(rawConfig.Config, &factory)
-		if err != nil {
-			return err
-		}
-
-		config = factory
-
-	default:
+	if !ok {
 		return fmt.Errorf("unknown access token issuer type: %s", rawConfig.Type)
 	}
 
-	c.Config = config
+	err = decode(rawConfig.Config, &factory)
+	if err != nil {
+		return err
+	}
+
+	c.Config = factory
 
 	return nil
 }
 
 // AccessTokenIssuerFactory creates a new auth.AccessTokenIssuer.
 type AccessTokenIssuerFactory interface {
+	New() AccessTokenIssuerFactory
 	CreateAccessTokenIssuer() (auth.AccessTokenIssuer, error)
 	Validate() error
 }
@@ -56,6 +82,10 @@ type jwtAccessTokenIssuer struct {
 	Issuer         string        `mapstructure:"issuer"`
 	PrivateKeyFile string        `mapstructure:"privateKeyFile"`
 	Expiration     time.Duration `mapstructure:"expiration"`
+}
+
+func (c jwtAccessTokenIssuer) New() AccessTokenIssuerFactory {
+	return jwtAccessTokenIssuer{}
 }
 
 func (c jwtAccessTokenIssuer) CreateAccessTokenIssuer() (auth.AccessTokenIssuer, error) {
