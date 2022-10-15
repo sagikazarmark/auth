c098874cd8ac6f1558f51cafe693ba24dac4bc7c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-10-15 17:30:56 +0200

    feat: introduce factory registry for authenticator config
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- config/authenticator.go
+++ config/authenticator.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"sync"
 
 	"golang.org/x/exp/maps"
 	"gopkg.in/yaml.v3"
@@ -10,6 +11,34 @@ import (
 	"github.com/distribution-auth/auth/auth/authn"
 	"github.com/distribution-auth/auth/pkg/slices"
 )
+
+var (
+	authenticatorFactoriesMu sync.RWMutex
+	authenticatorFactories   = make(map[string]AuthenticatorFactory)
+)
+
+// RegisterAuthenticatorFactory makes an AuthenticatorFactory available by the provided name in configuration.
+//
+// If RegisterAuthenticatorFactory is called twice with the same name or if factory is nil,
+// it panics.
+func RegisterAuthenticatorFactory(name string, factory AuthenticatorFactory) {
+	authenticatorFactoriesMu.Lock()
+	defer authenticatorFactoriesMu.Unlock()
+
+	if factory == nil {
+		panic("registering authenticator factory: factory is nil")
+	}
+
+	if _, dup := authenticatorFactories[name]; dup {
+		panic("registering authenticator factory: registration called twice for factory " + name)
+	}
+
+	authenticatorFactories[name] = factory
+}
+
+func init() {
+	RegisterAuthenticatorFactory("user", userAuthenticator{})
+}
 
 // Authenticator is the configuration for an auth.PasswordAuthenticator.
 type Authenticator struct {
@@ -27,29 +56,27 @@ func (c *Authenticator) UnmarshalYAML(value *yaml.Node) error {
 
 	c.Type = rawConfig.Type
 
-	var config AuthenticatorFactory
+	authenticatorFactoriesMu.RLock()
+	factory, ok := authenticatorFactories[rawConfig.Type]
+	authenticatorFactoriesMu.RUnlock()
 
-	switch rawConfig.Type {
-	case "user":
-		var factory userAuthenticator
-
-		err := decode(rawConfig.Config, &factory)
-		if err != nil {
-			return err
-		}
-
-		config = factory
-	default:
+	if !ok {
 		return fmt.Errorf("unknown authenticator type: %s", rawConfig.Type)
 	}
 
-	c.Config = config
+	err = decode(rawConfig.Config, &factory)
+	if err != nil {
+		return err
+	}
+
+	c.Config = factory
 
 	return nil
 }
 
 // AuthenticatorFactory creates a new auth.PasswordAuthenticator.
 type AuthenticatorFactory interface {
+	New() AuthenticatorFactory
 	CreateAuthenticator() (auth.PasswordAuthenticator, error)
 	Validate() error
 }
@@ -63,6 +90,10 @@ type user struct {
 	Username     string            `mapstructure:"username"`
 	PasswordHash string            `mapstructure:"passwordHash"`
 	Attrs        map[string]string `mapstructure:"attributes"`
+}
+
+func (c userAuthenticator) New() AuthenticatorFactory {
+	return userAuthenticator{}
 }
 
 func (c userAuthenticator) CreateAuthenticator() (auth.PasswordAuthenticator, error) {
