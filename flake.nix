9de15d2570da0ab7067c3cf187bd8883eb29d714
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-09-12 02:59:52 +0200

    feat: add flake and upgrade Go to 1.21
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Authentication library implementing the Docker (Distribution) Registry Auth specification";
+
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+        devenv.shells = {
+          default = {
+            languages = {
+              go.enable = true;
+              go.package = pkgs.go_1_21;
+            };
+
+            packages = with pkgs; [
+              just
+            ];
+
+            # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+            containers = pkgs.lib.mkForce { };
+          };
+
+          ci = devenv.shells.default;
+        };
+      };
+    };
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/distribution-auth/auth
 
-go 1.19
+go 1.21.0
 
 require (
 	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7
